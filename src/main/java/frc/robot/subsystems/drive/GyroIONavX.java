d8fe66579d933e2957b01453748e2a1688747095
Dhrish Karangula <dhrishkarangula@frc4322.com>
2023-11-22 22:50:37 -0800

    prevent nullPointerException due to time taken for gyro to initialize

--- src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.SPI;
 import frc.robot.NavX.AHRS;
-import frc.utility.OrangeMath;
 
 public class GyroIONavX implements GyroIO {
     private AHRS gyro;
@@ -23,7 +23,14 @@ public class GyroIONavX implements GyroIO {
         // yaw values used in Drive.java
         inputs.yawAngleDeg = -gyro.getAngle();
         inputs.yawVelocityDegPerSec = -gyro.getRate();
-        inputs.gyroYawRotation = gyro.getRotation2d();
+        
+        // prevent nullPointer for rotation2d input because gyro takes time to initialize
+        if (inputs.connected) {
+            inputs.gyroYawRotation = gyro.getRotation2d();
+        }
+        else {
+            inputs.gyroYawRotation = new Rotation2d();
+        }
     }
 
     public void setAngleAdjustment(double adjustment) {
