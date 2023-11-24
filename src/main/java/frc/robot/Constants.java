2530ecfd25dabe5309b468fbeb9cdc8ac3a1ec80
Dhrish Karangula <dhrishkarangula@frc4322.com>
2023-11-24 14:32:18 -0800

    get rid of unused spinout constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,8 +26,6 @@ public final class Constants {
   public static final boolean gyroEnabled = true;
   public static final boolean joysticksEnabled = true;
   public static final boolean xboxEnabled = true;
-  public static final boolean spinoutCenterEnabled = true;  // center rotate burst of power
-  public static final boolean spinoutCornerEnabled = true;
 
   public static final class Demo {
     public enum DriveMode {
