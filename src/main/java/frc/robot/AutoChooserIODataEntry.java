6cbb4832d181ee41793a17e27f1acf46111f5f6d
Ryan Vu <ryanvu@frc4322.com>
2024-01-10 20:27:15 -0800

    Initialize autoChooser and autoArrayList in constructor

--- src/main/java/frc/robot/AutoChooserIODataEntry.java
+++ src/main/java/frc/robot/AutoChooserIODataEntry.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.Drive;
 import frc.utility.Auto;
@@ -14,8 +13,8 @@ import frc.utility.OrangeSendableChooser;
 public class AutoChooserIODataEntry implements AutoChooserIO{
   private ShuffleboardTab customizationTab;
   private SendableChooser<Integer> positionChooser;
-  private ArrayList<Auto> autoArrayList = new ArrayList<Auto>();
-  private OrangeSendableChooser<Command> autoChooser = new OrangeSendableChooser<Command>();
+  private ArrayList<Auto> autoArrayList;
+  private OrangeSendableChooser<Command> autoChooser;
   private final PathPlannerManager ppManager;
   private int selectedPosition;
   
@@ -27,7 +26,7 @@ public class AutoChooserIODataEntry implements AutoChooserIO{
     customizationTab = Shuffleboard.getTab("Autos");
 
     // Widgets for Autos
-    positionChooser = new SendableChooser<Integer>();
+    positionChooser = new OrangeSendableChooser<Integer>();
     positionChooser.addOption("1", 1);
     positionChooser.addOption("2", 2);
     positionChooser.addOption("3", 3);
@@ -37,6 +36,9 @@ public class AutoChooserIODataEntry implements AutoChooserIO{
     positionChooser.addOption("7", 7);
     positionChooser.addOption("8", 8);
     positionChooser.addOption("9", 9);
+
+    autoChooser = new OrangeSendableChooser<Command>();
+    autoArrayList = new ArrayList<Auto>();
 
     customizationTab.add("Position Chooser", positionChooser).withWidget(BuiltInWidgets.kSplitButtonChooser)
       .withPosition(0, 0).withSize(9, 2);
